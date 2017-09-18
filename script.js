dca1c2978c751643669d0b33ddbbe8269d3c8f57
Morgan N <mzartdesigns@gmail.com>
2017-09-18 13:36:00 -0400

    Added randomization of colors

--- script.js
+++ script.js
@@ -12,16 +12,19 @@ var colors = [
 var rectangles = document.querySelectorAll(".rectangle");
 
 //RGB current color
-var pickedColor = colors[3];
+var pickedColor = currentColor();
 
 //RGB color title at the top
 var colorDisplay = document.querySelector("#colorDisplay");
+
+//Message that displays right or wrong choice
+var messageDisplay = document.querySelector("#message");
 
 //Displays the name of the current color at the top
 colorDisplay.textContent = pickedColor;
 
 //Loops through the color array and changes the color of the rectangles based on the i position
-for(var i = 0; i < rectangles.length; i++) {
+for(var i = 0; i < rectangles.length; i++){
   //add initial colors to rectangles
   rectangles[i].style.backgroundColor = colors[i];
 
@@ -29,12 +32,26 @@ for(var i = 0; i < rectangles.length; i++) {
   rectangles[i].addEventListener("click", function(){
     //grab color of clicked rectable
     var clickedColor = this.style.backgroundColor;
-
+    //compare color to pickedColor
     if(clickedColor === pickedColor){
-      alert("Correct!");
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
     } else {
       this.style.backgroundColor = "#f1f1f1";
+      messageDisplay.textContent = "Try Again";
     }
-    //compare color to pickedColor
-  })
+  });
+}
+
+function changeColors(color) {
+  //loop through all rectangles
+  for(var i = 0; i < rectangles.length; i++){
+    //change each color to match given color
+    rectangles[i].style.backgroundColor = color;
+  }
+}
+
+function currentColor(){
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
